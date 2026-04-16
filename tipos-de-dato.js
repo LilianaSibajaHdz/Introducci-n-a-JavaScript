6b911ae0c361d5c4fad3b8536d1ee9284cae4e09
LilianaSibajaHdz <lilianasibajahdez1106@gmail.com>
2026-04-16 16:37:32 -0600

    Feat: Mejora formato de salida en tipos-de-dato.js

--- tipos-de-dato.js
+++ tipos-de-dato.js
@@ -1,12 +1,14 @@
 // Ejemplos de tipos de datos
-console.log(typeof 42);
-console.log(typeof 'Veinticinco');
-console.log(typeof -666);
-console.log(typeof true);
-console.log(typeof 0);
-console.log(typeof '');
-console.log(typeof null);
-console.log(typeof undefined);
-console.log(typeof false);
+console.log("El valor 42 es de tipo:", typeof 42);
+console.log("El valor 'Veinticinco' es de tipo:", typeof 'Veinticinco');
+console.log("El valor -666 es de tipo:", typeof -666);
+console.log("El valor true es de tipo:", typeof true);
+console.log("El valor 0 es de tipo:", typeof 0);
+console.log("El valor '' es de tipo:", typeof '');
+console.log("El valor null es de tipo:", typeof null);
+console.log("El valor undefined es de tipo:", typeof undefined);
+console.log("El valor false es de tipo:", typeof false);
 
 // Mis propios ejemplos
+console.log("El valor 3.14 es de tipo:", typeof 3.14);
+console.log("El valor 'Hola' es de tipo:", typeof 'Hola');
